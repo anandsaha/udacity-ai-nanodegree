58a067f872f85cfb33a0a47b1d30839b31a2baa1
Anand Saha <anandsaha@gmail.com>
2017-05-17 04:27:23 +0530

    Implementation of solution to iterative deepening

--- AIND-Isolation/game_agent.py
+++ AIND-Isolation/game_agent.py
@@ -411,7 +411,11 @@ class AlphaBetaPlayer(IsolationPlayer):
         try:
             # The try/except block will automatically catch the exception
             # raised when the timer is about to expire.
-            return self.alphabeta(game, self.search_depth)
+
+            depth = 0
+            while True:
+                depth += 1
+                best_move = self.alphabeta(game, depth)
 
         except SearchTimeout:
             pass  # Handle any actions required after timeout as needed
